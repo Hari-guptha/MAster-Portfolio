d7a5093d086ba682de42c1511a2b99d9898eaf55
Hari-guptha <harihariguptha@gmail.com>
2024-10-12 02:04:27 +0530

    update

--- src/components/Models/Arts.jsx
+++ src/components/Models/Arts.jsx
@@ -12,7 +12,7 @@ export function Arts(props) {
   texture.offset.y = 1;
 
   return (
-    <group {...props} dispose={null} position={[0, -2, 0]} scale={0.5}>
+    <group {...props} dispose={null} position={[0, -1.5, 0]} scale={0.5}>
       <group position={[0, 4.427, 0.215]} rotation={[0, 0.003, 0]} scale={[4.045, 2.608, 0.137]}>
         <mesh geometry={nodes.Cube008.geometry} material={materials['Material.013']} />
         <mesh geometry={nodes.Cube008_1.geometry} material={materials['Material.003']} />
@@ -29,10 +29,9 @@ export function Arts(props) {
       <mesh geometry={nodes.Cube019.geometry} material={materials['Material.004']} position={[0, 4.427, 0.215]} rotation={[0, 0.003, 0]} scale={[4.045, 2.608, 0.137]} />
       <mesh geometry={nodes.Cube010.geometry} material={materials['Material.005']} position={[0, 4.427, 0.215]} rotation={[0, 0.003, 0]} scale={[4.045, 2.608, 0.137]} />
       <mesh geometry={nodes.Cube011.geometry} material={materials['Material.006']} position={[0, 4.427, 0.215]} rotation={[0, 0.003, 0]} scale={[4.045, 4.25, 0.137]} />
-      <mesh geometry={nodes.Cube024.geometry} material={materials['Material.017']} position={[0, 4.427, 0.215]} rotation={[0, 0.003, 0]} scale={[4.045, 2.608, 0.137]} >
-        <meshBasicMaterial map={texture} />
-      </mesh>
+      <mesh geometry={nodes.Cube024.geometry} material={materials['Material.017']} position={[0, 4.427, 0.215]} rotation={[0, 0.003, 0]} scale={[4.045, 2.608, 0.137]} />
       <mesh geometry={nodes.Cube025.geometry} material={materials['Material.018']} position={[0, 4.427, 0.215]} rotation={[0, 0.003, 0]} scale={[4.045, 4.25, 0.137]} />
+      <mesh geometry={nodes.videoboard.geometry} material={materials['Material.001']} position={[0, 4.427, 0.215]} rotation={[0, 0.003, 0]} scale={[4.045, 2.608, 0.137]} />
       <mesh geometry={nodes.Cube030.geometry} material={materials['Material.010']} position={[0, 4.427, 0.215]} rotation={[0, 0.003, 0]} scale={[4.045, 2.608, 0.137]} />
     </group>
   )
